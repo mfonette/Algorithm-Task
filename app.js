a9336edfce4550359fd808356e0293102b218330
mfonette <ettemfon98@gmail.com>
2022-01-28 19:02:26 +0100

    Parenthesis Validation Task

--- app.js
+++ app.js
@@ -1,91 +1,55 @@
-// const validateParenthesis = (s) => {
-//     let singleParenthesis =[];
-//     // let x = s[0];
-//     // console.log(x)
-//  strParenthesis = s.split(',');
-// //  console.log(strParenthesis)
-//  let x = strParenthesis;
-// //  console.log(x)
-// for(let i=0; i<strParenthesis.length; i++) {
-//     console.log(x[i])
-//     singleParenthesis.push(x[i]);
-//     for (let b=1; b<strParenthesis; b++){
-//         console.log(x[b])
-//     if (x!==i){
-//         if ((i === '(' && x === ")")){
-//             console.log("its valid")
-//         }
-//     }
-//     let parenthesis = x[i];
-//     console.log (parenthesis);
-//     let isValid = true;
-//     for (let b=1; b<strParenthesis; b++) {
-//         // if((i === '(' && b === ")")||(i === '{' && b === "}")|| (i === '[' && b === "]") ){
-//         //     console.log(i);
-//         //     console.log(b);
-//         //     isValid;
-//         // }
-
-//     }
-//     if(isValid) {
-//         ans += parenthesis;
-//     }
-// }
-// }
-//     return ans;
-// }
-
-
-
 const validateParenthesis = (validationElm) => {
-let openBracketArr = [];
-let strValidationElm = validationElm.split('');
-// console.log(strValidationElm)
+const listOfOpenBracket = ['[', '{', '('];
+const listOfClosedBracket = [']', '}', ')'];
+const openBracketArr = [];
+const strValidationElm = validationElm.split('');
 
+const bracketMap = {
+    '(':')',
+    '[':']',
+    '{':'}'
+}
+
+// console.log(bracketMap['('])
 for (let i = 0; i< strValidationElm.length; i++) {
     // console.log(i)
-    let prvOpenBracket = []
-    prvOpenBracket.push(openBracketArr[openBracketArr.length-1]);
+    const prvOpenBracket = openBracketArr[openBracketArr.length-1];
+    const currentChar = strValidationElm[i];
     // console.log(prvOpenBracket)
-    // if (validationElm.length % 2 !== 0){
-    //     console.log("it is not valid")
-    //     return false
-    // }
-    if (strValidationElm[i] =='{' || strValidationElm[i] == '['|| strValidationElm[i] == '(') {
-        openBracketArr.push(strValidationElm[i])
-        // console.log(openBracketArr)
-        // console.log(prvOpenBracket)
+    if (strValidationElm.length % 2 !== 0){
+        console.log("it is not validd");
+        return false
     }
-    // else if ( (strValidationElm[i] =='}' || strValidationElm[i] == ']'|| strValidationElm[i] == ')')){
-    //     if((openBracketArr[openBracketArr.length-1] == "{" || openBracketArr[openBracketArr.length-1] == "[" || openBracketArr[openBracketArr.length-1] == "(")){
-    //         console.log("found a pair")
-    //     }
+     else if (listOfOpenBracket.some(b => b === currentChar)) {
+        openBracketArr.push(currentChar);
+    }
+    else if(bracketMap[prvOpenBracket] === currentChar){
+        openBracketArr.pop();
+    }
+    // if (currentChar=='{' || currentChar== '['|| currentChar== '(') {
+    //     openBracketArr.push(currentChar);
+    //     // console.log(openBracketArr);
+    //     // console.log(prvOpenBracket);
     // }
-    else if ((strValidationElm[i] =='}' && openBracketArr[openBracketArr.length-1] == "{") || (strValidationElm[i] == ']' && openBracketArr[openBracketArr.length-1] == "[")
-     ||(strValidationElm[i] == ')' && openBracketArr[openBracketArr.length-1] == "(")){
-         console.log("pair found")
-     }
-    // else if ((prvOpenBracket == "[" && strValidationElm[i] == "]") ||(prvOpenBracket == "{" && strValidationElm[i] == "}")||(prvOpenBracket == "(" && strValidationElm[i] == ")")) {
-    //     console.log(prvOpenBracket)
-    //     // prvOpenBracket.pop();
-    // }
-    // else{
-    //     return false;
-    // }
-}
-// if (openBracketArr.length == [] ){
-//     console.log("it is valid")
-//     return true
-// }
-// else{
-//     console.log("it is not valid")
-//     return false
-// }
-}
 
+    // else if ((currentChar=='}' && prvOpenBracket == "{") || (currentChar== ']' && prvOpenBracket == "[")
+    //  ||(currentChar== ')' && prvOpenBracket == "(")){
+    //      openBracketArr.pop();
+    //  }
+}
+if (openBracketArr.length === 0){
+    console.log("it is valid");
+    return true
+}
+else{
+    console.log("it is not valid");
+    return false
+}
+}
+validateParenthesis("()}")
 validateParenthesis("()")
-// validateParenthesis("()[]{}")
-// validateParenthesis("(}")
-// validateParenthesis("([)]")
-// validateParenthesis("{[]}")
-// validateParenthesis("{,},[,],(,)")
+validateParenthesis("()[]{}")
+validateParenthesis("(}")
+validateParenthesis("([)]")
+validateParenthesis("{[]}")
+validateParenthesis("[[[]")
